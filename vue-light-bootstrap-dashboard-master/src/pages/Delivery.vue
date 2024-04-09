4f7c1882c966869f74032a06343c8fce48bb1f0d
qkralsgml78 <qkralsgml78@gmail.com>
2024-04-09 17:56:31 +0900

    [Add] 지도 수정 중

--- vue-light-bootstrap-dashboard-master/src/pages/Delivery.vue
+++ vue-light-bootstrap-dashboard-master/src/pages/Delivery.vue
@@ -22,22 +22,22 @@
                 </div>
 
                 <div class="col-12">
-                  <card class="card-plain">
-                    <template slot="header">
-                      <h4 class="card-title">도착하는 판매처 주소</h4>
-                      <p class="card-category">{{ customerName }}</p>
-                    </template>
-                    <div class="table-responsive">
-                      <table class="table table-hover">
-                        <tbody>
-                            <tr v-for="(item, index) in Customers.data" :key="index">
-                                <th scope="row">{{ item.label }}</th>
-                                <td>{{ item.value }}</td>
-                            </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </card>
+                    <card class="card-plain">
+                        <template slot="header">
+                            <h4 class="card-title">도착하는 판매처 주소</h4>
+                            <p class="card-category">{{ customerName }}</p>
+                        </template>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <tbody>
+                                    <tr v-for="(item, index) in Customers.data" :key="index">
+                                        <th scope="row">{{ item.label }}</th>
+                                        <td>{{ item.value }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </card>
                 </div>
             </div>
         </div>
@@ -56,6 +56,8 @@ export default {
         return {
             warehouseName: '', // 창고 코드를 저장할 변수
             customerName: '',
+            warehouseA: '',
+            customerA: '',
             Deliveries: {
                 data: []
             },
@@ -69,53 +71,84 @@ export default {
         this.fetchContacts();
     },
     methods: {
-        fetchSContacts() {
+        async fetchSContacts() {
             const orderNumber = '2'; // 주문 번호를 설정합니다
 
-            axios.get(`http://localhost:8080/api/orders/id/BUS002/${orderNumber}`)
-                .then(response => {
-                    // 응답 데이터에서 창고 정보를 가정하고 추출합니다
-                    const storageContact = response.data.storageContact;
-                    if (storageContact) {
-                        // 창고 코드를 설정합니다
-                        this.warehouseName = storageContact.contactName; // 실제 데이터에 맞게 키를 변경해야 할 수도 있습니다
-                        // 연락처 정보를 설정합니다
-                        this.Deliveries.data = [
-                            { label: '창고 이름', value: storageContact.contactName },
-                            { label: '창고 연락처', value: storageContact.customerPhone },
-                            { label: '창고 주소', value: storageContact.contactAddress }
-                        ];
-                    } else {
-                        console.error("창고 정보를 가져오는 데 실패했습니다.");
-                    }
-                })
-                .catch(error => {
-                    console.error("거래처 목록을 가져오는 데 실패했습니다.", error);
-                });
+            try {
+                const response = await axios.get(`http://localhost:8080/api/orders/id/BUS002/${orderNumber}`);
+                const storageContact = response.data.storageContact;
+                if (storageContact) {
+                    this.warehouseName = storageContact.contactName;
+                    this.warehouseA = storageContact.contactAddress;
+                    this.Deliveries.data = [
+                        { label: '창고 이름', value: storageContact.contactName },
+                        { label: '창고 연락처', value: storageContact.customerPhone },
+                        { label: '창고 주소', value: storageContact.contactAddress }
+                    ];
+                    this.fetchCoordinates(); // 올바르게 주소가 설정되었을 때 좌표를 가져옵니다.
+                } else {
+                    console.error("창고 정보를 가져오는 데 실패했습니다.");
+                }
+            } catch (error) {
+                console.error("거래처 목록을 가져오는 데 실패했습니다.", error);
+            }
         },
-        fetchContacts() {
-          const orderNumber = '2'; // 주문 번호를 설정합니다
+        async fetchContacts() {
+            const orderNumber = '2'; // 주문 번호를 설정합니다
 
-          axios.get(`http://localhost:8080/api/orders/id/BUS002/${orderNumber}`)
-              .then(response => {
-                  // 응답 데이터에서 판매처 정보를 추출합니다
-                  const customerContact = response.data.customerContact;
-                  if (customerContact) {
-                      this.customerName = customerContact.contactName;
-                      // 판매처 정보를 설정합니다
-                      this.Customers.data = [
-                          { label: '고객 이름', value: customerContact.contactName },
-                          { label: '고객 연락처', value: customerContact.customerPhone },
-                          { label: '고객 주소', value: customerContact.contactAddress }
-                      ];
-                  } else {
-                      console.error("판매처 정보를 가져오는 데 실패했습니다.");
-                  }
-              })
-              .catch(error => {
-                  console.error("판매처 목록을 가져오는 데 실패했습니다.", error);
-              });
-        }
+            try {
+                const response = await axios.get(`http://localhost:8080/api/orders/id/BUS002/${orderNumber}`);
+                const customerContact = response.data.customerContact;
+                if (customerContact) {
+                    this.customerName = customerContact.contactName;
+                    this.customerA = customerContact.contactAddress;
+                    this.Customers.data = [
+                        { label: '고객 이름', value: customerContact.contactName },
+                        { label: '고객 연락처', value: customerContact.customerPhone },
+                        { label: '고객 주소', value: customerContact.contactAddress }
+                    ];
+                } else {
+                    console.error("판매처 정보를 가져오는 데 실패했습니다.");
+                }
+            } catch (error) {
+                console.error("판매처 목록을 가져오는 데 실패했습니다.", error);
+            }
+        },
+        async fetchCoordinates() {
+            const warehouseItem = warehouseA;
+            const customerItem = this.customerA;
+            console.log(this.warehouseItem);
+            console.log(this.Deliveries.data)
+
+            
+            if (warehouseItem && customerItem) { // 주소 데이터가 올바르게 설정되었는지 확인합니다.
+                const warehouseAddress = warehouseItem.value;
+                const customerAddress = customerItem.value;
+
+                const warehouseCoordinates = await this.getCoordinates(warehouseAddress);
+                const customerCoordinates = await this.getCoordinates(customerAddress);
+
+                this.$emit('coordinates-fetched', { warehouseCoordinates, customerCoordinates });
+            } else {
+                console.error('주소 데이터가 올바르게 설정되지 않았습니다.');
+            }
+        },
+        async getCoordinates(address) {
+            try {
+                const response = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURI(address)}`, {
+                    headers: {
+                        Authorization: 'KakaoAK f74e4f598452c26325500e84e020e0fc',
+                    },
+                });
+                return {
+                    latitude: response.data.documents[0].y,
+                    longitude: response.data.documents[0].x,
+                };
+            } catch (error) {
+                console.error('Failed to fetch coordinates:', error);
+                return null;
+            }
+        },
     }
 }
 </script>
