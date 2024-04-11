688eee86d231fe8e15214316166339fa9ff4b320
ae-chae <aechae11@gmail.com>
2024-04-11 13:08:37 +0900

    [Add]

--- vue-light-bootstrap-dashboard-master/src/pages/TableList7.vue
+++ vue-light-bootstrap-dashboard-master/src/pages/TableList7.vue
@@ -11,17 +11,13 @@
               <!-- <button @click="saveOrderChanges"></button> -->
             </template>
 
-            <!-- <base-dropdown :initial-title="dropdownTitle" @item-selected="updateTitle" ref="dropdown">
+            <base-dropdown :initial-title="dropdownTitle" ref="dropdown">
               <template v-slot:default>
                 <a v-for="code in storageCodes" :key="code" class="dropdown-item" @click="selectStorageCode(code)">
                   {{ code }}
                 </a>
               </template>
-            </base-dropdown> -->
-            <select v-model="selectedStorageCode">
-      <option disabled value="">Please select one</option>
-      <option v-for="code in storageCodes" :key="code" :value="code">{{ code }}</option>
-    </select>
+            </base-dropdown>
 
             <input type="text" v-model="searchQuery" placeholder="상품명 검색" class="form-control"/>
             <l-table class="table-hover table-striped"
@@ -87,12 +83,7 @@ export default {
       if (newVal !== '') {
         this.fetchGoodsList(newVal);
       }
-    },
-    searchQuery(newVal) {
-    if (newVal !== '' && this.selectedStorageCode !== '') {
-      this.fetchGoodsList(this.selectedStorageCode);
     }
-  }
   },
   mounted() {
     this.fetchOrderList();
@@ -100,9 +91,6 @@ export default {
   methods: {
     updateTotalAmount(amount) {
     this.totalAmount = amount;
-  },
-  updateTitle(selectedItem) {
-    this.dropdownTitle = selectedItem; // 선택된 항목으로 타이틀 업데이트
   },
     selectStorageCode(code) {
       this.selectedStorageCode = code;
@@ -126,126 +114,71 @@ export default {
         });
     },
     fetchGoodsList(storageCode) {
-      axios.get(`http://localhost:8080/api/total/storage/${storageCode}`)
-      .then(response => {
-
-      })
-
-      
     axios.get(`http://localhost:8080/api/total/storage/${storageCode}`)
       .then(response => {
-        const filteredData = response.data.filter(goods => goods.goodsName.toLowerCase().includes(this.searchQuery.toLowerCase()));
-      this.goodsList.data = filteredData.map(goods => ({
-        ...goods,
-        visible: false,
-        '상품 코드': goods.goodsCode,
-        '상품명': goods.goodsName,
-        '상품 등급': goods.goodsGrade,
-        '주문 가능 수량': goods.totalQuantity,
-        '가격(BOX)': Math.ceil(goods.inputPrice * (100 + goods.marginRate) / 100),
-        '주문 수량': 0,
-        '금액(원)' : 0
-      }));
-    })
-    .catch(error => {
-      console.error(`There was an error fetching the goods list for storageCode ${storageCode}:`, error);
-    });
+        this.goodsList.data = response.data.map(goods => ({
+          ...goods,
+          visible: false, // Initially, rows are not visible
+          '상품 코드': goods.goodsCode,
+          '상품명': goods.goodsName,
+          '상품 등급': goods.goodsGrade,
+          '주문 가능 수량': goods.totalQuantity,
+          '가격(BOX)': Math.ceil(goods.inputPrice * (100 + goods.marginRate) / 100),
+          '주문 수량': 0,
+          '금액(원)' : 0
+        }));
+      })
+      .catch(error => {
+        console.error(`There was an error fetching the goods list for storageCode ${storageCode}:`, error);
+      });
   },
   makeRowPermanent(goodsCode) {
     const item = this.goodsList.data.find(item => item['상품 코드'] === goodsCode);
     if (item) item.visible = true;
   },
-  //   submitOrder() {
-  //     if (this.totalAmount <= 0) {
-  //   window.alert("총 금액이 0원입니다. 주문을 등록할 수 없습니다.");
-  //   return;
-  // }
-  //     //const customerCode = this.$route.params.customerCode;
-  //   const orderData = {
-  //     customerCode: "CON001", 
-  //     storageCode: this.selectedStorageCode,
-  //     orderPrice: this.totalAmount, 
-  //     adjustmentStatus: "미정산"
-  //   };
-  //   axios.post('/api/orders/post', orderData)
-  //     .then(response => {
-  //       console.log('Order created!', response.data);
-  //       const createdOrderNumber = response.data.orderNumber;
-  //       console.log(createdOrderNumber);
-
-  //       const nonZeroItems = this.goodsList.data.filter(item => item['주문 수량'] > 0);
-
-  //       nonZeroItems.forEach(item => {
-  //       const orderProductData = {
-  //         orderNumber: createdOrderNumber,
-  //         goodsCode: item['상품 코드'],
-  //         goodsGrade: item['상품 등급'],
-  //         orderQuantity: item['주문 수량'],
-  //         orderPrice: item['금액(원)'] 
-  //       };
-  //       axios.post('/api/order/detail/post', orderProductData)
-  //         .then(res => {
-  //           console.log('Item added with the order number', res.data);
-  //         })
-  //         .catch(err => {
-  //           console.error('Error posting item:', err);
-  //         });
-  //     });
-  //   })
-  //   .catch(error => {
-  //     console.error('Error creating order:', error);
-  //   });
-  // }
-  submitOrder() {
-  if (this.totalAmount <= 0) {
+    submitOrder() {
+      if (this.totalAmount <= 0) {
     window.alert("총 금액이 0원입니다. 주문을 등록할 수 없습니다.");
     return;
   }
+      //const customerCode = this.$route.params.customerCode;
+    const orderData = {
+      customerCode: "CON001", 
+      storageCode: this.selectedStorageCode,
+      orderPrice: this.totalAmount, 
+      adjustmentStatus: "미정산",
+      orderStatus: "주문 완료"
+    };
+    axios.post('/api/orders/post', orderData)
+      .then(response => {
+        console.log('Order created!', response.data);
+        const createdOrderNumber = response.data.orderNumber;
+        console.log(createdOrderNumber);
 
-  const orderData = {
-    customerCode: "CON001", 
-    storageCode: this.selectedStorageCode,
-    orderPrice: this.totalAmount, 
-    adjustmentStatus: "미정산",
-    orderStatus: "주문 완료"
-  };
+        const nonZeroItems = this.goodsList.data.filter(item => item['주문 수량'] > 0);
 
-  axios.post('/api/orders/post', orderData)
-    .then(response => {
-      console.log('Order created!', response.data);
-      const createdOrderNumber = response.data.orderNumber;
-      console.log(createdOrderNumber);
-
-      const nonZeroItems = this.goodsList.data.filter(item => item['주문 수량'] > 0);
-
-      // 모든 비어있지 않은 항목에 대해 주문 세부 정보를 게시합니다.
-      const postOrderDetailsPromises = nonZeroItems.map(item => {
+        nonZeroItems.forEach(item => {
         const orderProductData = {
           orderNumber: createdOrderNumber,
           goodsCode: item['상품 코드'],
           goodsGrade: item['상품 등급'],
           orderQuantity: item['주문 수량'],
-          orderPrice: item['금액(원)']
+          orderPrice: item['금액(원)'] 
         };
+        axios.post('/api/order/detail/post', orderProductData)
+          .then(res => {
+            console.log('Item added with the order number', res.data);
+          })
+          .catch(err => {
+            console.error('Error posting item:', err);
+          });
 
-        return axios.post('/api/order/detail/post', orderProductData);
       });
-
-      // 모든 주문 세부 정보 요청이 성공적으로 완료되면,
-      Promise.all(postOrderDetailsPromises).then(() => {
-        window.alert("주문이 성공적으로 등록되었습니다.");
-        // 주문 등록 후 필요한 상태 초기화 또는 페이지 이동 로직 추가
-      }).catch(err => {
-        console.error('Error posting order details:', err);
-        window.alert("주문 세부 정보 등록 중 문제가 발생했습니다.");
-      });
-
     })
     .catch(error => {
       console.error('Error creating order:', error);
-      window.alert("주문 등록 중 문제가 발생했습니다.");
     });
-}
+  }
 
   }
 };
