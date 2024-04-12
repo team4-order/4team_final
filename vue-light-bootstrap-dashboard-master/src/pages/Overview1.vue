a1b676d037e6b8851eca2e51253c352e687f8d16
ae-chae <aechae11@gmail.com>
2024-04-12 17:54:42 +0900

    [Add]

--- vue-light-bootstrap-dashboard-master/src/pages/Overview1.vue
+++ vue-light-bootstrap-dashboard-master/src/pages/Overview1.vue
@@ -15,7 +15,7 @@
                 <h4 class="card-title">{{ weeklySales }} 원</h4>
               </div>
               <div slot="footer">
-                <i class="fa fa-calendar-o"></i>     for a week   
+                <i class="fa fa-calendar-o"></i> for a week
               </div>
             </stats-card>
           </div>
@@ -30,12 +30,12 @@
                 <h4 class="card-title">{{ weeklyReadyOrders }} 건</h4>
               </div>
               <div slot="footer">
-              <i class="fa fa-refresh"></i>Updated now
-            </div>
+                <i class="fa fa-refresh"></i>Updated now
+              </div>
             </stats-card>
           </div>
 
-          
+
 
           <div class="col-xl-3 col-md-6">
             <stats-card>
@@ -71,22 +71,67 @@
       </Card>
 
       <div class="row">
-        <div class="col-md-8">
-          <chart-card :chart-data="lineChart.data" :chart-options="lineChart.options"
+        <div class="col-md-8" v-if="isDataLoaded">
+          <chart-card ref="salesChart" :chart-data="lineChart.data" :chart-options="lineChart.options"
             :responsive-options="lineChart.responsiveOptions">
+
             <template slot="header">
-              <h4 class="card-title">Users Behavior</h4>
-              <p class="card-category">24 Hours performance</p>
+              <h4 class="card-title">매출 현황</h4>
+              <p class="card-category">(단위: 만 원)</p>
             </template>
             <template slot="footer">
               <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Click
-                <i class="fa fa-circle text-warning"></i> Click Second Time
+                <i class="fa fa-circle text-info"></i> 총 매출
               </div>
+              <hr>
+              <!-- <div class="stats">
+                <i class="fa fa-history"></i> Updated 3 minutes ago
+              </div> -->
+            </template>
+          </chart-card>
+        </div>
+
+        <div class="col-md-3">
+          <card>
+            <template slot="header">
+              <h5 class="title">정산 요청 리스트</h5>
+              <p class="category"></p>
+            </template>
+            <l-table :data="tableData.data" :columns="tableData.columns">
+            </l-table>
+            <div class="footer">
               <hr>
               <div class="stats">
                 <i class="fa fa-history"></i> Updated 3 minutes ago
+              </div>
+            </div>
+          </card>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <chart-card :chart-data="barChartData" :chart-options="barChart.options"
+            :responsive-options="barChart.responsiveOptions" chart-type="Bar" ref="customerOrdersChart">
+
+            <template slot="header">
+              <h4 class="card-title">고객별 주문 현황</h4>
+              <p class="card-category"></p>
+              <select v-model="selectedCustomerCode" class="form-control">
+                <option disabled value="">고객명</option>
+                <option v-for="customer in customers" :key="customer.contactCode" :value="customer.contactCode">
+                  {{ customer.contactName }}
+                </option>
+              </select>
+            </template>
+            <template slot="footer">
+              <div class="legend">
+                <i class="fa fa-circle text-info"></i> 주문 수
+              </div>
+              <hr>
+              <div class="stats">
+                <i class="fa fa-check"></i> Data information certified
               </div>
             </template>
           </chart-card>
@@ -111,46 +156,8 @@
             </template>
           </chart-card>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col-md-6">
-          <chart-card :chart-data="barChart.data" :chart-options="barChart.options"
-            :chart-responsive-options="barChart.responsiveOptions" chart-type="Bar">
-            <template slot="header">
-              <h4 class="card-title">2014 Sales</h4>
-              <p class="card-category">All products including Taxes</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Tesla Model S
-                <i class="fa fa-circle text-danger"></i> BMW 5 Series
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-check"></i> Data information certified
-              </div>
-            </template>
-          </chart-card>
-        </div>
 
-        <div class="col-md-6">
-          <card>
-            <template slot="header">
-              <h5 class="title">정산 요청 리스트</h5>
-              <p class="category"></p>
-            </template>
-            <l-table :data="tableData.data" :columns="tableData.columns">
-            </l-table>
-            <div class="footer">
-              <hr>
-              <div class="stats">
-                <i class="fa fa-history"></i> Updated 3 minutes ago
-              </div>
-            </div>
-          </card>
-
-        </div>
       </div>
     </div>
   </div>
@@ -171,6 +178,10 @@ export default {
   },
   data() {
     return {
+      isDataLoaded: false,
+      customers: [],
+      selectedCustomerCode: '',
+      barChartData: { labels: [], series: [] },
       weeklyReadyOrders: 0,
       weeklydeliveryOrders: 0,
       weeklyCompletedOrders: 0,
@@ -185,16 +196,34 @@ export default {
       },
       lineChart: {
         data: {
-          labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-          series: [
-            [287, 385, 490, 492, 554, 586, 698, 695],
-            [67, 152, 143, 240, 287, 335, 435, 437],
-            [23, 113, 67, 108, 190, 239, 307, 308]
-          ]
+          labels: [],
+          series: []
         },
         options: {
-          low: 0,
-          high: 800,
+          scales: {
+            xAxes: [{
+              ticks: {
+                fontSize: 10, // Smaller font size
+                fontColor: '#333', // Adjust color for better visibility
+                fontStyle: 'bold' // Makes text bold
+              }
+            }],
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: "단위: 만 원",
+                fontStyle: 'bold'
+              }
+            }]
+          },
+          layout: {
+            padding: {
+              left: 20,  // Increase left padding to ensure y-axis labels are visible
+              right: 20,
+              top: 10,
+              bottom: 10  // Adjust right padding if necessary
+            }
+          },
           showArea: false,
           height: '245px',
           axisX: {
@@ -205,14 +234,15 @@ export default {
           showPoint: true,
           fullWidth: true,
           chartPadding: {
-            right: 50
+            right: 50,
+            left: 10  // Make sure to adjust this value to prevent y-axis label clipping
           }
         },
         responsiveOptions: [
           ['screen and (max-width: 640px)', {
             axisX: {
-              labelInterpolationFnc(value) {
-                return value[0]
+              labelInterpolationFnc: function (value) {
+                return value[0];
               }
             }
           }]
@@ -246,57 +276,142 @@ export default {
       },
       tableData: {
         columns: ['고객명', '정산 요청 금액'],
-        data: [] 
+        data: []
+      }
+    }
+  },
+  watch: {
+    selectedCustomerCode(newCode, oldCode) {
+      if (newCode && newCode !== oldCode) {
+        this.fetchCustomerOrders(newCode);
       }
     }
   },
   mounted() {
-  this.fetchOrderList();
-  this.fetchAdjustmentReqList();
+    this.fetchOrderList();
+    this.fetchAdjustmentReqList();
+    this.fetchMonthlySalesData();
+    this.fetchCustomerList();
+    this.$nextTick(() => {
+    if (this.$refs.customerOrdersChart && this.$refs.customerOrdersChart.update) {
+      this.$refs.customerOrdersChart.update();
+    }
+  });
   },
   methods: {
-    // other methods...
-    fetchOrderList() {
-  const businessId = this.$route.params.businessId;
-  axios.get(`http://localhost:8080/api/orders/id/${businessId}`)
-    .then(response => {
-      const data = response.data;
-      const startOfWeek = new Date();
-      startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + (startOfWeek.getDay() === 0 ? -6 : 1));
-      startOfWeek.setHours(0, 0, 0, 0);
-
-      let totalSales = 0;
-      let completedOrders = 0;
-      let deliveryOrders = 0;
-      let readyOrders = 0;
-
-      data.forEach(order => {
-        const orderDate = new Date(order.orderDate);
-        if (orderDate >= startOfWeek) {
-          switch (order.orderStatus) {
-            case '배송 완료':
-              completedOrders++;
-              totalSales += order.orderPrice; // 주문 상태가 '배송 완료'인 경우에 매출액을 더합니다.
-              break;
-            case '배송 중':
-              deliveryOrders++;
-              break;
-            case '출고 준비 중':
-              readyOrders++;
-              break;
-          }
+    fetchCustomerOrders(customerCode) {
+      axios.get(`http://localhost:8080/api/orders/chart3/${customerCode}`)
+        .then(response => {
+          const labels = response.data.map(item => `${item.year.toString().slice(-2)}-${item.month.toString().padStart(2, '0')}`);
+          const series = response.data.map(item => item.count);
+          this.updateCustomerOrdersChart(labels, series);
+        })
+        .catch(error => console.error('Error fetching customer orders:', error));
+    },
+    updateCustomerOrdersChart(labels, series) {
+      this.barChartData = { labels: labels, series: [series] }; // Update the whole object to ensure reactivity
+      this.isDataLoaded = true; // This will trigger any v-if in your template that depends on the chart data
+      this.$nextTick(() => {
+        if (this.$refs.customerOrdersChart) {
+          this.$refs.customerOrdersChart.update();  // Assuming your chart component exposes an `update` method
         }
       });
+    },
+    fetchCustomerList() {
+      const businessId = this.$route.params.businessId;
+      axios.get(`http://localhost:8080/api/orders/chart2/${businessId}`)
+        .then(response => {
+          this.customers = response.data.map(customer => ({
+            contactName: customer.contactName,
+            contactCode: customer.contactCode
+          }));
+        })
+        .catch(error => {
+          console.error("고객 목록을 가져오는 데 실패했습니다.", error);
+        });
+    },
+    async fetchMonthlySalesData() {
+      try {
+        const businessId = this.$route.params.businessId;
+        const response = await axios.get(`http://localhost:8080/api/orders/chart1/${businessId}`);
+        const months = response.data.map(item => `${item.year.toString().slice(-2)}/${item.month.toString().padStart(2, '0')}`);
+        const sales = response.data.map(item => item.price);
+        this.updateSalesChart(months, sales);
+      } catch (error) {
+        console.error("Failed to fetch monthly sales data:", error);
+      }
+    },
+    updateSalesChart(months, sales) {
+      const minPrice = Math.min(...sales);
+      const maxPrice = Math.max(...sales);
 
-      this.weeklyCompletedOrders = completedOrders;
-      this.weeklyReadyOrders = readyOrders;
-      this.weeklydeliveryOrders = deliveryOrders;
-      this.weeklySales = totalSales;
-    })
-    .catch(error => {
-      console.error("Failed to fetch orders:", error);
-    });
-},
+      // Scale the max price a bit higher for better visualization
+      const maxChartValue = maxPrice + (maxPrice - minPrice) * 0.1;
+
+      // Update the chart data
+      this.lineChart.data.labels = months;
+      this.lineChart.data.series = [sales];
+      this.isDataLoaded = true;
+      // Ensure the chart updates or redraws if necessary
+      if (this.$refs.salesChart && this.$refs.salesChart.update) {
+        this.$refs.salesChart.update();  // Call update if it is a function provided by the chart lib
+      }
+
+      this.lineChart.options.axisY = {
+        low: minPrice,
+        high: maxChartValue,
+        onlyInteger: true, 
+        scaleMinSpace: 20 
+      };
+
+      this.lineChart.options.scales.yAxes[0].scaleLabel.labelString = "단위: 만 원";
+
+      this.isDataLoaded = true;
+      this.$refs.salesChart.update();
+
+
+    },
+    fetchOrderList() {
+      const businessId = this.$route.params.businessId;
+      axios.get(`http://localhost:8080/api/orders/id/${businessId}`)
+        .then(response => {
+          const data = response.data;
+          const startOfWeek = new Date();
+          startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + (startOfWeek.getDay() === 0 ? -6 : 1));
+          startOfWeek.setHours(0, 0, 0, 0);
+
+          let totalSales = 0;
+          let completedOrders = 0;
+          let deliveryOrders = 0;
+          let readyOrders = 0;
+
+          data.forEach(order => {
+            const orderDate = new Date(order.orderDate);
+            if (orderDate >= startOfWeek) {
+              switch (order.orderStatus) {
+                case '배송 완료':
+                  completedOrders++;
+                  totalSales += order.orderPrice; // 주문 상태가 '배송 완료'인 경우에 매출액을 더합니다.
+                  break;
+                case '배송 중':
+                  deliveryOrders++;
+                  break;
+                case '출고 준비 중':
+                  readyOrders++;
+                  break;
+              }
+            }
+          });
+
+          this.weeklyCompletedOrders = completedOrders;
+          this.weeklyReadyOrders = readyOrders;
+          this.weeklydeliveryOrders = deliveryOrders;
+          this.weeklySales = totalSales;
+        })
+        .catch(error => {
+          console.error("Failed to fetch orders:", error);
+        });
+    },
     fetchAdjustmentReqList() {
       const businessId = this.$route.params.businessId;
       axios.get(`http://localhost:8080/api/orders/req/${businessId}`)
@@ -305,11 +420,11 @@ export default {
             '고객명': item.contactName,
             '고객코드': item.contactCode
           }));
-    })
-    .catch(error => {
-      console.error("Failed to fetch adjustment requests:", error);
-    });
-}
+        })
+        .catch(error => {
+          console.error("Failed to fetch adjustment requests:", error);
+        });
+    }
     // continue with other methods...
   }
 }
