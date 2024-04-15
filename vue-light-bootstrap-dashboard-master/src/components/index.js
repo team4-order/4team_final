20899274a9997c6650047c052ddf4cc12f62a27c
CoffeerLatte <todty2312@gmail.com>
2024-04-15 15:21:47 +0900

    commit fixed

--- vue-light-bootstrap-dashboard-master/src/components/index.js
+++ vue-light-bootstrap-dashboard-master/src/components/index.js
@@ -8,7 +8,7 @@ import Table from './Table.vue'
 
  import Card1 from './Cards/Card1.vue'
  import ChartCard1 from './Cards/ChartCard1.vue'
-// import StatsCard1 from './Cards/StatsCard1.vue'
+ import StatsCard1 from './Cards/StatsCard1.vue'
 import Card from './Cards/Card.vue'
 import ChartCard from './Cards/ChartCard.vue'
 import StatsCard from './Cards/StatsCard.vue'
@@ -19,9 +19,9 @@ let components = {
   BaseCheckbox,
   Radio,
   BaseInput,
-  Card,
-  ChartCard,
-  StatsCard,
+  Card,Card1,
+  ChartCard,ChartCard1,
+  StatsCard, StatsCard1,
   Table,
   BaseDropdown,
   SidebarPlugin
--- vue-light-bootstrap-dashboard-master/src/routes/routes.js
+++ vue-light-bootstrap-dashboard-master/src/routes/routes.js
@@ -30,7 +30,7 @@ import OrderApply from 'src/pages/TableList7.vue'
 import Overview1 from 'src/pages/Overview1.vue'
 import OpenAI from 'src/pages/OpenAI.vue'
 /*import { component } from 'vue/types/umd.js'*/
-/*import CustomerList from 'src/components/CustomerList.vue'*/
+/*import CustomerList1 from 'src/components/CustomerList.vue'*/
 import BCustomerList from 'src/pages/BCustomerList.vue'
 import BAdjustment from 'src/pages/BAdjustment.vue' // 경로 수정
 import CustomerList from 'src/pages/CustomerList.vue'
