caa81a6cad553b147ede0fb50d35061817de90c4
CoffeerLatte <todty2312@gmail.com>
2024-04-24 12:39:09 +0900

    fix route

--- vue-light-bootstrap-dashboard-master/src/main.js
+++ vue-light-bootstrap-dashboard-master/src/main.js
@@ -59,7 +59,7 @@ router.beforeEach((to, from, next) => {
       sessionStorage.removeItem('cuser');
       this.authenticated = false;
       // 값이 없으면 로그인 페이지로 리다이렉트
-      next({ path: '/login', replace: true });
+      next(this.$router.replace("/login"));
     }
   } else {
     // 인증이 필요없는 페이지는 바로 다음으로 넘어갑니다.
